2d67a4f5904de0535f05e1d29e6148ec4c05d241
Furkan Veli İnal <ifurkanveli@gmail.com>
2023-03-01 13:49:20 +0300

    TestNG

---
+++ src/test/java/day11_POM_assertions/C04_HardAssertSoftAssert.java
@@ -0,0 +1,55 @@
+package day11_POM_assertions;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class C04_HardAssertSoftAssert {
+
+    @Test
+    public void hardAssertTesti(){
+        Assert.assertTrue(5>4);
+
+        Assert.assertFalse(5==6);
+
+        Assert.assertEquals(5,5);
+
+        Assert.assertNotEquals(5,1);
+    }
+
+    @Test
+    public void softAssertionTesti(){
+
+        /*
+        JUnit teki Assert class'i testNG de de vardir
+        Ancak bir test methodun'da birden fazla Failed olan Assertion varsa
+        ilk failed olan assertion'da calismayi durdurup exception firlattigindan
+        tum hatayi bir defada gorup, hepsini duzeltme sansimiz olmaz
+
+        TestNG bunun icin bir alternatif olusturmus
+        Bu alternatifte Assert class'inda ki static methodlari kullanmak yerine
+
+        SoftAssert class'indan obje olusturup
+        o obje uzerinden istedigimiz testleri yapiyoruz
+
+        softAssert objesi ile yapilan assertion'lar failed olsada testimiz calismaya devam eder
+        ancak testlerin sonuclarini  gormek istedigimizde
+        softAssert.assertAll() ile tum yaptigi testleri raporlamasini soyleyebiliriz
+
+        assertAll() methodunun calistigi satirda exception olabilir
+         */
+        SoftAssert softAssert= new SoftAssert();
+
+
+        softAssert.assertTrue(5>6,"true testi failed");
+
+        softAssert.assertFalse(5==5,"false testi failed");
+
+        softAssert.assertEquals(5,6,"Equels testi failed");
+
+        softAssert.assertNotEquals(5,5,"Not Equals testi failed");
+
+        softAssert.assertAll();
+    }
+}
---
+++ src/test/java/day11_POM_assertions/C05_softAssert.java
@@ -0,0 +1,45 @@
+package day11_POM_assertions;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C05_softAssert {
+    @Test
+    public void  amazonTest(){
+        /*
+        Genel olarak Test edin denirse HardAssert
+        dogrulayin (verify) denirse softAssert kastedilir
+         */
+
+        // amazon anasayfaya gidip dogru sayfaya gittigimizi dogrulayin (verify)
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        String expectedIcerik="amazon";
+        String actualUrl=Driver.getDriver().getCurrentUrl();
+
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertTrue(actualUrl.contains(expectedIcerik),"url ramazon icermiyor");
+
+        //Nutella icin arama yapip.sonuclarin Nutella icerdigini test edin
+        AmazonPage amazonPage=new AmazonPage();
+
+        amazonPage.aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+        expectedIcerik="Nutella";
+        String actualAramaSonucYazisi=amazonPage.aramaSonucElementi.getText();
+        softAssert.assertTrue(actualAramaSonucYazisi.contains(expectedIcerik),"Arama sonucu nutella icermiyor");
+
+        // ilk urune tiklayip. uruun isminde nutella gectigini dogrulayin
+        amazonPage.ilkUrun.click();
+        expectedIcerik="Nutella";
+        String actualIlkUrunIsim=amazonPage.ilkUrunIsimElementi.getText();
+
+        softAssert.assertTrue(actualIlkUrunIsim.contains(expectedIcerik),"arama sonucu nutella icermiyor");
+
+        softAssert.assertAll();
+        Driver.closeDriver();
+    }
+}
